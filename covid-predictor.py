24a72ae31490302be20e21d63e34fa10edf0315e
Rafay Rasool <rafay_rasool30@hotmail.com>
2020-03-14 16:44:35 +0000

    updated with more logic

--- covid-predictor.py
+++ covid-predictor.py
@@ -1,9 +1,10 @@
 # !/usr/bin/env/python3
 import json
+import sys
 from decimal import Decimal
 from numpy import mean
 
-with open('./data/covid.json') as f:
+with open('./data/'+ sys.argv[1] +'.json') as f:
   data = json.load(f)
 
 # define a value of a to multiply with the first value of cases in for loop
@@ -28,23 +29,36 @@ for x,y in data.items():
     exp_list.append(exp)
     a = curcases
 
-#Store the value of last recorded cases:
+#store the value of last recorded cases:
 cases_today = a
 #pick the last 15 days exponential increase as usable data and remove all others
-exp_list = exp_list[-15:]
+exp_list = exp_list[-10:]
 
 #calculate the average exponentail from the last 15 days
 avg = mean(exp_list)
 
-# limit the output to two decimal places
-avg = (Decimal(exp)).quantize(TWOPLACES)
-
 #predict the expected increase in cases for the following day:
 predicted_increase = avg * cases_today
 
-#Calculate total number of cases for the next day
+#calculate total number of cases for the next day
 total_cases = cases_today + predicted_increase
 
-print ("Total cases today : {0} :: Predicted increase tomorrow: {1} :: Total cases for tomorrow: {2}".format(cases_today,predicted_increase,total_cases))
+#convert average exponential increase into readable percentable
+avg_exp_percentage = avg * 100
 
+#remove decimal values
+avg_exp_percentage = (Decimal(avg_exp_percentage)).quantize(TWOPLACES)
+avg_exp_percentage = avg_exp_percentage // 1
+
+predicted_increase = (Decimal(predicted_increase)).quantize(TWOPLACES)
+predicted_increase = predicted_increase // 1
+
+total_cases = (Decimal(total_cases)).quantize(TWOPLACES)
+total_cases = total_cases // 1
+
+print ("Country: {0}".format(sys.argv[1]))
+print ("Total cases today: {0}".format(cases_today))
+print ("Predicted increase tomorrow: {0}".format(predicted_increase))
+print ("Total predicted confirmed cases for tomorrow: {0}".format(total_cases))
+print ("Exponential increase: {0} percent".format(avg_exp_percentage))
 
