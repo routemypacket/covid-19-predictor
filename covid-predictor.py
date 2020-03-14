85a8697fe18eaf062518394b49616e3b225209fe
Rafay Rasool <rafay_rasool30@hotmail.com>
2020-03-14 17:27:47 +0000

    added one week - two weeks and one month prediction

--- covid-predictor.py
+++ covid-predictor.py
@@ -23,6 +23,7 @@ for x,y in data.items():
     #calculate exponential value
     exp = curcases / a
     exp = (Decimal(exp)).quantize(TWOPLACES)
+    # store the 1.x value seperately
     exp = exp - 1
     exp = str(exp)
     exp = float(exp)
@@ -46,6 +47,22 @@ total_cases = cases_today + predicted_increase
 #convert average exponential increase into readable percentable
 avg_exp_percentage = avg * 100
 
+#convert avg to 1.xx value
+avg_a = avg + 1
+
+#calculate one week prediction
+avg_power_seven = avg_a ** 7
+avg_one_week = cases_today * avg_power_seven
+
+#calculate one week prediction
+avg_power_fourteen = avg_a ** 14
+avg_two_weeks = cases_today * avg_power_fourteen
+
+#calculate one week prediction
+avg_power_thirty = avg_a ** 30
+avg_one_month = cases_today * avg_power_thirty
+
+
 #remove decimal values
 avg_exp_percentage = (Decimal(avg_exp_percentage)).quantize(TWOPLACES)
 avg_exp_percentage = avg_exp_percentage // 1
@@ -56,9 +73,21 @@ predicted_increase = predicted_increase // 1
 total_cases = (Decimal(total_cases)).quantize(TWOPLACES)
 total_cases = total_cases // 1
 
+avg_one_week = (Decimal(avg_one_week)).quantize(TWOPLACES)
+avg_one_week = avg_one_week // 1
+
+avg_two_weeks = (Decimal(avg_two_weeks)).quantize(TWOPLACES)
+avg_two_weeks = avg_two_weeks // 1
+
+avg_one_month = (Decimal(avg_one_month)).quantize(TWOPLACES)
+avg_one_month = avg_one_month // 1
+
+
 print ("Country: {0}".format(sys.argv[1]))
 print ("Total cases today: {0}".format(cases_today))
 print ("Predicted increase tomorrow: {0}".format(predicted_increase))
 print ("Total predicted confirmed cases for tomorrow: {0}".format(total_cases))
-print ("Exponential increase: {0} percent".format(avg_exp_percentage))
-
+print ("Avergae exponential increase: {0} percent".format(avg_exp_percentage))
+print ("Predicted cases in a week: {0}".format(avg_one_week))
+print ("Predicted cases in two weeks: {0}".format(avg_two_weeks))
+print ("Predicted cases in one month: {0}".format(avg_one_month))
